7d83aba30dd3dccd85641eee546e3f82e3d23fa3
hcps-hallocbr1 <hcps-hallocbr1@180-18HALLOCBR1.hcps.internal>
2016-05-27 16:33:44 -0400

    Commented Coloring

--- src/map/Coloring.java
+++ src/map/Coloring.java
@@ -17,28 +17,28 @@ import java.util.Scanner;
  */
 public class Coloring{
     
-    HashMap colors = new HashMap<String, Color>();
+    HashMap colors = new HashMap<String, Color>();//makes a HashMap to store colors by name
     
     
     public void voteStates(Scanner scan){
-        scan.nextLine();
-        while(scan.hasNext()){
-            String[] votes = scan.nextLine().split(",");
-            String name = votes[0];
+        scan.nextLine();//spacing
+        while(scan.hasNext()){//scans all input
+            String[] votes = scan.nextLine().split(",");//gets the data and puts it into an array
+            String name = votes[0];//name of thing having datat recorded on it
                     
-            int rVotes = Integer.parseInt(votes[1]);
-            int dVotes = Integer.parseInt(votes[2]);
-            int iVotes = Integer.parseInt(votes[3]);
+            int rVotes = Integer.parseInt(votes[1]);//republican votes
+            int dVotes = Integer.parseInt(votes[2]);//democrat votes
+            int iVotes = Integer.parseInt(votes[3]);//independent votes
             int sum = iVotes+rVotes+dVotes;
-            Color temp = new Color((255*rVotes)/sum, (255*iVotes)/sum, (255*dVotes)/sum);
-            colors.put(name, temp);
+            Color temp = new Color((255*rVotes)/sum, (255*iVotes)/sum, (255*dVotes)/sum);//makes the color
+            colors.put(name, temp);//saves the data in the hashmap
             //System.out.println(name+temp);
         }
     }
     
     
-    public Color getMapColor(String s){
-        return (Color) colors.get(s);
+    public Color getMapColor(String s){//returns the data held, takes the name of the target
+        return (Color) colors.get(s);//returns the color associated with the name
         
     }
 }
